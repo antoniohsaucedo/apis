706ba18cacb0aee220c39e5cf32c531efb84452f
Antonio Hernandez <antoniohsaucedo@gmail.com>
2021-11-16 18:21:27 -0700

    Add files via upload

---
+++ apis/swapi/main.js
@@ -0,0 +1,11 @@
+
+
+
+function showRandom() {
+	console.log('button clicked');
+	
+}
+
+
+let residentsButton = document.querySelector('button');
+residentsButton.addEventListener('click', showRandom);
